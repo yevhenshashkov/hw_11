eae0e64688d37b9b7eda9153208918674edcbe2d
YevhenShashkov <tehnolog3000@gmail.com>
2025-03-19 15:48:36 +0200

    frst

---
+++ main.js
@@ -0,0 +1,6 @@
+const myDiv = document.querySelector("#myDiv")
+const myBtn = document.querySelector("button")
+myDiv.classList.add("firstColorStyle")
+myBtn.addEventListener("click", () => {
+    myDiv.classList.toggle("secondColorStyle",)
+    });
